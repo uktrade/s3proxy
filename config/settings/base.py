d1402f4768c8b791ec81900938786661e2b20c2f
Ross Miller <github@decodedcollective.com>
2021-05-10 12:08:38 +0100

    Remove static files app

--- config/settings/base.py
+++ config/settings/base.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles",
     "file_proxy",
     "user",
     "authbroker_client",
