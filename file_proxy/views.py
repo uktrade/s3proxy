df50f60c12d94ac2a697958fc7ebe8d000b4a8ba
Ross Miller <github@decodedcollective.com>
2021-05-10 16:25:08 +0100

    Flake8 and Black

--- file_proxy/views.py
+++ file_proxy/views.py
@@ -4,7 +4,11 @@ from django.conf import settings
 from django.http import StreamingHttpResponse
 
 
-s3 = boto3.client("s3")
+s3 = boto3.client(
+    "s3",
+    aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+)
 
 
 def file_proxy(request, s3_path):
