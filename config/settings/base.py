64e07995852591f4b0f3106b892cefe7f863fcfc
Ross Miller <github@decodedcollective.com>
2021-05-10 11:46:36 +0100

    Put back static files app

--- config/settings/base.py
+++ config/settings/base.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "django.contrib.staticfiles",
     "file_proxy",
     "user",
     "authbroker_client",
