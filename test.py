29d992c9190c0fdf4b10c7ce454d2e26cd12e7ff
marcelkornblum <me@marcelkornblum.com>
2023-01-23 14:40:09 +0000

    updated reqs and env vars used in tests

--- test.py
+++ test.py
@@ -862,13 +862,15 @@ def create_application(
             'SSO_URL': 'http://127.0.0.1:8081/',
             'SSO_CLIENT_ID': 'the-client-id',
             'SSO_CLIENT_SECRET': 'the-client-secret',
-            'AWS_S3_REGION': 'us-east-1',
+            'AWS_S3_BUCKET': 'my-bucket',
+            'AWS_DEFAULT_REGION': 'us-east-1',
             'AWS_ACCESS_KEY_ID': aws_access_key_id,
             'AWS_SECRET_ACCESS_KEY': 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY',
-            'AWS_S3_ENDPOINT': 'http://127.0.0.1:9000/my-bucket/',
             'AWS_S3_HEALTHCHECK_KEY': healthcheck_key,
         }
     )
+        # os.environ.get('KEY_PREFIX', None),
+
 
     def wait_until_started():
         for i in range(0, max_attempts):
